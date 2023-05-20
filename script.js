e8297f3ad1d3aa6f42f0aa3706de3642553d0f7b
Liliia Baieva <555blv@gmail.com>
2023-05-20 16:17:59 +0300

    refactoring userList2, prin only when type some characters in search line

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const url = 'https://jsonplaceholder.typicode.com/users';
+const userList2 = document.getElementById('userList2');
+let userDetailsDiv = document.getElementById('userDetails');
+const searchInput = document.getElementById('searchInput');
+const users = [];
 
 fetch(url)
     .then(res => res.json())
@@ -12,70 +16,34 @@ fetch(url)
     })
     .catch(err => console.log(err));
 
-const userList2 = document.getElementById('userList2');
-let userDetailsDiv = document.getElementById('userDetails');
-const searchInput = document.getElementById('searchInput');
-// let filteredUsers = [];
-const users = [];
 fetch(url)
     .then(res => res.json())
     .then((usersGetting) => {
-        displayUsers(usersGetting);     
         usersGetting.forEach(user => users.push(user));      
     })
     .catch((error) => {console.log("Error fetching user", error);});
     
-console.log(users);
 
 searchInput.addEventListener("input", () => {
     const searchTerm = searchInput.value.toLowerCase();
-    filteredUsers = users.filter((user) =>
-        user.name.toLowerCase().includes(searchTerm) //'Hello world'.includes('ell') - true ищет в строке подстроку
-    );
-    displayUsers(filteredUsers);
+    if(searchTerm){
+        filteredUsers = users.filter((user) =>
+            user.name.toLowerCase().includes(searchTerm) //'Hello world'.includes('ell') - true ищет в строке подстроку
+            );
+            displayUsers(filteredUsers);
+    }
+
 });
-
-// ==========================================================
-
-// const usersWithDetails = [];
-// const usersData = [];
-// fetch(url)
-//     .then(res => res.json)
-//     .then((usersGet) => {
-//         usersGet.forEach(user => {
-//             dataUser = JSON.parse(user);
-//             usersData.push(dataUser);
-//         })
-
-//         // displayUserDetails(usersGet);
-//         // for(let i = 0; i < usersGet; ++i){
-//         //     usersWithDetails.add(usersGet[i]);
-//         // }
-//     });
- 
-// console.log(users[0].name);
-
 
 userList2.addEventListener('click', (e) => {
     let userGet = e.target.textContent;
     console.log(userGet);
     users.forEach(user => {        
         if(user.name === userGet){
-            // console.log(user.email);
             displayUserDetails(user);
             };
         });  
 });
-
-// class User {
-//     constructor(name, userName, email, phone){
-//         this.name = name;
-//         this.userName = userName;
-//         this.email = email;
-//         this.phone = phone;
-//     };
-// }
-    
 
 function displayUsers(users){
     userList2.innerHTML = '';
@@ -88,10 +56,6 @@ function displayUsers(users){
 }
 
 function displayUserDetails(user){
-
-    // console.log(user.name + ', ' + user.username + ', ' + user.email + ', ' + user.phone)
-    
-    // userDetailsDiv.innerHTML = '';
     
     const nameHeading = document.createElement('h2');
     nameHeading.innerHTML = user.name;
@@ -110,6 +74,3 @@ function displayUserDetails(user){
     phone.innerHTML = `<b>Phone: </b> ${user.phone}`;
     userDetailsDiv.append(phone);
 }
-
-
-
